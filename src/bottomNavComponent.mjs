6e1390fcd2e8249f10b3fc3571edff108e35c8ee
Joy Shaheb <istiaque.joy@northsouth.edu>
2022-06-30 00:22:08 +0600

    work in porgress

---
+++ src/bottomNavComponent.mjs
@@ -0,0 +1,9 @@
+let payment = document.querySelector(".bottomNav__payment");
+let receive = document.querySelector(".bottomNav__receive");
+let send = document.querySelector(".bottomNav__send");
+let loan = document.querySelector(".bottomNav__loan");
+
+payment.addEventListener("click", () => {});
+receive.addEventListener("click", () => {});
+send.addEventListener("click", () => {});
+loan.addEventListener("click", () => {});
