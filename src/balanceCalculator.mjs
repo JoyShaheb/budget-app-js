87a577d89c3e68fea0a36e0b37164b5aedbabfad
Joy Shaheb <istiaque.joy@northsouth.edu>
2022-07-05 09:23:49 +0600

    color class fixed

--- src/balanceCalculator.mjs
+++ src/balanceCalculator.mjs
@@ -3,8 +3,10 @@ let totalBalance = document.querySelector(".balanceComponent__balance");
 export let balanceCalculator = (data) => {
   let calculations = data.map((item) => item.amount).reduce((x, y) => x + y, 0);
   if (calculations > 0) {
+    totalBalance.classList.remove("red");
     totalBalance.classList.add("green");
   } else {
+    totalBalance.classList.remove("green");
     totalBalance.classList.add("red");
   }
   return (totalBalance.innerHTML = `$ ${calculations}`);
